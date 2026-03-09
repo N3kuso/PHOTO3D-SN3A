9a32fc72697aa3d8c95e8023b0f592040d3a662a
N3kuso <matthieu.picot83@gmail.com>
2026-03-09 10:29:29 +0100

    added exercice 2

--- TP1/main.py
+++ TP1/main.py
@@ -18,6 +18,33 @@ def main():
     axes.tick_params(axis='x', colors='red') # X Axis graduation color
     axes.tick_params(axis='y', colors='green') # Y Axis graduation color
     axes.tick_params(axis='z', colors='blue') # Z Axis graduation color
+    
+    origin_point=(0,0,0)
+
+    # X points
+    x_pos=(10,0,0)
+    x_neg=(-10,0,0)
+
+    # Y points
+    y_pos=(0,10,0)
+    y_neg=(0,-10,0)
+
+    # Z points
+    z_pos=(0,0,10)
+    z_neg=(0,0,-10)
+
+    # Draw X referential axis
+    plt.plot([origin_point[0] , x_pos[0]],[origin_point[1] , x_pos[1]],[origin_point[2] , x_pos[2]], color='red', linestyle='solid')
+    plt.plot([origin_point[0] , x_neg[0]],[origin_point[1] , x_neg[1]],[origin_point[2] , x_neg[2]], color='red', linestyle='dashed')
+
+    # Draw Y referential axis
+    plt.plot([origin_point[0] , y_pos[0]],[origin_point[1] , y_pos[1]],[origin_point[2] , y_pos[2]], color='green', linestyle='solid')
+    plt.plot([origin_point[0] , y_neg[0]],[origin_point[1] , y_neg[1]],[origin_point[2] , y_neg[2]], color='green', linestyle='dashed')
+
+    # Draw X referential axis
+    plt.plot([origin_point[0] , z_pos[0]],[origin_point[1] , z_pos[1]],[origin_point[2] , z_pos[2]], color='blue', linestyle='solid')
+    plt.plot([origin_point[0] , z_neg[0]],[origin_point[1] , z_neg[1]],[origin_point[2] , z_neg[2]], color='blue', linestyle='dashed')
+
     # Display the 3D plotting window
     plt.show()
 
