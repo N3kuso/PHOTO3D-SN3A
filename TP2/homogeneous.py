23dc0bafa70293ca7e15799fa50d18397227edf0
N3kuso <matthieu.picot83@gmail.com>
2026-03-12 10:07:51 +0100

    added exercise 6

--- TP2/homogeneous.py
+++ TP2/homogeneous.py
@@ -8,13 +8,31 @@ def toHomogeneous(v: tuple) -> np.array:
     Output
         result = np.array
 
-    Function that that takes in parameter a tuple v representing a vector expressed within homogeneous coordinates 
+    Function that that takes in parameter a tuple v representing a vector expressed within Euclidean coordinates 
+    and that returns a np.array representing the equivalent vector within homogeneous coordinates
+    """
+    result = np.array(list(v) + [1.0])
+    print(f"""Function toHomogeneous :
+Input > v : {v}
+Output > {result}
+          """)
+    return result
+
+def toEuclidean(v: tuple) -> np.array:
+    """
+    Input:
+        v : tuple
+    Output
+        result = np.array
+
+    Function hat takes in parameter a tuple v representing a vector expressed within homogeneous coordinates 
     and that returns a np.array representing the equivalent vector within Euclidean coordinates
     """
-    result = np.array(list(v) + [1])
-    print(f"""Function toHomogeneous :
-        Input > v : {v}
-        Output > {result}
+    v=np.array(v)
+    result= v[:-1] / v[-1]
+    print(f"""Function toEuclidian :
+Input > v : {v}
+Output > {result}
           """)
     return result
 
@@ -22,5 +40,9 @@ def toHomogeneous(v: tuple) -> np.array:
 # Exercise 5
 v_euclidian = (1.0, 2.0, 3.0)
 v_homogeneous = toHomogeneous(v_euclidian)
+
+# Exercise 6
+v_homogeneous_bis=(2.0,4.0,6.0,2.0)
+v_euclidian_bis= toEuclidean(v_homogeneous_bis)
 
 
