4992769338d6e6bb6004f6583e4e1fb5ae391072
N3kuso <matthieu.picot83@gmail.com>
2026-03-12 10:57:32 +0100

    added exercise 7

--- TP2/homogeneous.py
+++ TP2/homogeneous.py
@@ -36,6 +36,26 @@ Output > {result}
           """)
     return result
 
+def translate_point_hc(point, alpha, beta, gamma):
+    """
+    Function that takes in parameter the homogeneous representation of a point 
+    and that return the homogeneous vector resulting of the translation (x,y,z) of 
+    along vector (alpha, beta, gamma)
+    """
+    T = np.array([[1,0,0,alpha],
+                 [0,1,0,beta],
+                 [0,0,1,gamma],
+                 [0,0,0,1]])
+    
+    result=T.dot(point.T)
+
+    print(f""" Function translate_point_hc :
+Translatation matrix : {T}
+Point : {point}
+Result : {result}
+            """)
+    return result
+
 ### MAIN ###
 # Exercise 5
 v_euclidian = (1.0, 2.0, 3.0)
@@ -44,5 +64,12 @@ v_homogeneous = toHomogeneous(v_euclidian)
 # Exercise 6
 v_homogeneous_bis=(2.0,4.0,6.0,2.0)
 v_euclidian_bis= toEuclidean(v_homogeneous_bis)
+
+# Exercise 7
+point=(4.0,3.0,2.0)
+point=toHomogeneous(point)
+translate_vector=(0.0,1.0,1.0)
+
+point_translated=toEuclidean(translate_point_hc(point, *translate_vector))
 
 
