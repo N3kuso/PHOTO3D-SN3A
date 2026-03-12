de3a6d0f67253e646f75839e433f8f3302efb523
N3kuso <matthieu.picot83@gmail.com>
2026-03-12 15:58:57 +0100

    added exercice 11

--- TP2/homogeneous.py
+++ TP2/homogeneous.py
@@ -167,6 +167,49 @@ Result : {result}
             """)
     return result
 
+def rot_point(point, omega, phi, kappa):
+    """
+Function that takes in parameter an homegeneous vector that represents a point (x,y,z) 
+and returns the homogeneous vector that represents the rotated point around the three axis X, Y and Z axis by angles omega, phi and kappa respectively (expressed in radians).
+    """
+    cx, sx = np.cos(omega), np.sin(omega)
+    cy, sy = np.cos(phi),   np.sin(phi)
+    cz, sz = np.cos(kappa), np.sin(kappa)
+
+    R = np.array([
+        [cz*cy, (cz*sy*sx)-(sz*cx), (cz*sy*cx)+(sz*sx), 0],
+        [sz*cy, (sz*sy*sx)+(cz*cx), (sz*sy*cx)-(cz*sx),  0],
+        [-sy, cy*sx, cy*cx,  0],
+        [0, 0,  0, 1]
+    ])
+
+    result = R.dot(point.T)
+
+    print(f""" Function rot_point :
+Rotation matrix : {R}
+Point : {point}
+Result : {result}
+            """)
+    return result
+
+def rot_point_comp(point, omega, phi, kappa):
+    """
+Function that takes in parameter an homegeneous vector that represents a point (x,y,z) 
+and returns the homogeneous vector that represents the rotated point around the three axis X, Y and Z axis by angles omega, phi and kappa respectively (expressed in radians).
+Using the comp version    
+    """
+
+    result = rot_x_point(point, omega)
+    result = rot_y_point(result, phi)
+    result = rot_z_point(result, kappa)
+
+    print(f""" Function rot_point_comp :
+Point : {point}
+Result : {result}
+            """)
+    
+    return result
+
 ### MAIN ###
 # Exercise 5
 v_euclidian = (1.0, 2.0, 3.0)
@@ -245,6 +288,27 @@ draw_referential(axes)
 
 axes.plot(px, py, pz,  marker='o', color='black', markersize=8, label="Original")
 axes.plot(*point_rotated, marker='o', color='blue',   markersize=8, label="Ry(π/8)")
+axes.legend()
+plt.show()
+
+# Exercise 11
+point=(4.0, 3.0, 2.0)
+point=toHomogeneous(point)
+rotation_angle_vector=(np.pi/6, np.pi/7,np.pi/8) #alpha, beta, gamma
+
+point_rotated=toEuclidean(rot_point(point, *rotation_angle_vector))
+point_rotated_comp=toEuclidean(rot_point_comp(point, *rotation_angle_vector))
+
+px, py, pz = toEuclidean(point)
+rx, ry, rz = point_rotated
+r2x, r2y, r2z = point_rotated_comp
+
+axes = init_3d_axes(title="Exercice 11")
+draw_referential(axes)
+
+axes.plot(px, py, pz,  marker='o', color='black', markersize=8, label="Original")
+axes.plot(*point_rotated, marker='o', color='red',   markersize=8, label="R(π/6, π/7, π/8)")
+axes.plot(*point_rotated_comp, marker='+', color='orange',   markersize=8, label="Rcomp(π/6, π/7, π/8)")
 axes.legend()
 plt.show()
 
