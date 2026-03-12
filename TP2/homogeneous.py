1d8869c08f9112f01c96b8476251bd5544ee36a6
N3kuso <matthieu.picot83@gmail.com>
2026-03-12 13:25:09 +0100

    added exercise 10

--- TP2/homogeneous.py
+++ TP2/homogeneous.py
@@ -143,7 +143,28 @@ Rotation matrix : {Ry}
 Point : {point}
 Result : {result}
             """)
+    return result
+
+def rot_z_point(point, kappa):
+    """
+Function that takes in parameter an homegeneous vector that represents a point (x,y,z)
+and returns the homogeneous vector that represents the rotated point around Z axis by an angle kappa (expressed in radians).
+    """
+    c, s = np.cos(kappa), np.sin(kappa)
+    Rz = np.array([
+        [c, -s, 0, 0],
+        [s, c, 0,  0],
+        [0, 0, 1,  0],
+        [0, 0,  0, 1]
+    ])
     
+    result = Rz.dot(point.T)
+
+    print(f""" Function rot_z_point :
+Rotation matrix : {Rz}
+Point : {point}
+Result : {result}
+            """)
     return result
 
 ### MAIN ###
@@ -206,6 +227,24 @@ draw_referential(axes)
 
 axes.plot(px, py, pz,  marker='o', color='black', markersize=8, label="Original")
 axes.plot(*point_rotated, marker='o', color='green',   markersize=8, label="Ry(π/7)")
+axes.legend()
+plt.show()
+
+# Exercise 10
+point=(4.0, 3.0, 2.0)
+point=toHomogeneous(point)
+rotation_angle=np.pi/8
+
+point_rotated=toEuclidean(rot_z_point(point, rotation_angle))
+
+px, py, pz = toEuclidean(point)
+rx, ry, rz = point_rotated
+
+axes = init_3d_axes(title="Exercice 10")
+draw_referential(axes)
+
+axes.plot(px, py, pz,  marker='o', color='black', markersize=8, label="Original")
+axes.plot(*point_rotated, marker='o', color='blue',   markersize=8, label="Ry(π/8)")
 axes.legend()
 plt.show()
 
