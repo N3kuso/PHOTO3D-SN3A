8d1e606ba48862fa681ce675b22bab8f03a1cb30
N3kuso <matthieu.picot83@gmail.com>
2026-03-13 13:11:21 +0100

    added exercice 1

---
+++ TP3/calibration_chessboard.py
@@ -0,0 +1,15 @@
+import numpy as np
+import cv2 as cv
+import glob
+import matplotlib.pyplot as plt
+
+def calibrate_chessboard(image_files):
+	print('Chessboard calibration')
+
+
+def main():
+	calibrate_chessboard(())
+
+
+if __name__ == "__main__":
+	main()
