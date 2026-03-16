f0973584e0f8a6086162d7f0ad0a9c5dc40677db
N3kuso <matthieu.picot83@gmail.com>
2026-03-16 10:20:25 +0100

    added exercices 2 & 3

--- TP3/calibration_chessboard.py
+++ TP3/calibration_chessboard.py
@@ -3,12 +3,26 @@ import cv2 as cv
 import glob
 import matplotlib.pyplot as plt
 
+
 def calibrate_chessboard(image_files):
-	print('Chessboard calibration')
+	print('Image list:')
+	for image_file in image_files:
+		print('  - '+image_file)
+		img = cv.imread(image_file)
+		gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+		plt.figure(image_file)
+		plt.imshow(img)
+	
+	plt.show()
 
 
 def main():
-	calibrate_chessboard(())
+
+	# Load images from data folder
+	image_files = glob.glob('data/google_pixel_6/chessboard/6x11/*.jpg')
+
+	calibrate_chessboard(image_files)
 
 
 if __name__ == "__main__":
